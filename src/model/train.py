1c52761592a9d1daa30c80e7c38b7bd36aac53aa
Johan K Norvik <92482357+jknmsft@users.noreply.github.com>
2023-03-20 14:23:42 -0500

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -50,7 +50,8 @@ def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
     LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
 
-    def parse_args():
+    
+def parse_args():
     # setup arg parser
     parser = argparse.ArgumentParser()
 
