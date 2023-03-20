d2e08aec027dbab1ca6a15572b32b8251368dc40
Johan K Norvik <92482357+jknmsft@users.noreply.github.com>
2023-03-20 14:26:37 -0500

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -5,7 +5,7 @@ import glob
 import os
 import mlflow
 import pandas as pd
-#import numpy as np
+# import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
