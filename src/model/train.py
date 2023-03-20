e9ab2b7e3470f10c5590980aec674e7d3eaf8f37
Johan K Norvik <92482357+jknmsft@users.noreply.github.com>
2023-03-20 14:19:00 -0500

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -5,7 +5,7 @@ import glob
 import os
 import mlflow
 import pandas as pd
-import numpy as np
+#import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
@@ -14,7 +14,6 @@ from sklearn.model_selection import train_test_split
 def main(args):
     # TO DO: enable autologging -- done
     mlflow.autolog()
-
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -38,8 +37,12 @@ def get_csvs_df(path):
 # TO DO: add function to split data  -- done
 
 def split_data(df):
-    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values,df['Diabetic'].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    X, y = \
+    df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness',\
+        'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']].values, \
+    df['Diabetic'].values
+    X_train, X_test, y_train, y_test = \
+    train_test_split(X, y, test_size=0.30, random_state=0)
     return X_train, X_test, y_train, y_test
     
 
@@ -47,8 +50,7 @@ def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
     LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
 
-
-def parse_args():
+    def parse_args():
     # setup arg parser
     parser = argparse.ArgumentParser()
 
@@ -63,6 +65,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
